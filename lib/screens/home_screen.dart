7bee9bfda832272f46220ec79ed1f0afc16a9c94
jrahme10 <jrahme10@gmail.com>
2023-03-02 22:43:09 +0200

    tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,120 +42,123 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: ClipRRect(
-        borderRadius: BorderRadius.circular(100),
-        child: Container(
-          height: 60,
-          width: 60,
-          color: const Color.fromARGB(150, 0, 0, 0),
-        ).blurred(
-          colorOpacity: 0.2,
-          borderRadius:
-              const BorderRadius.horizontal(right: Radius.circular(20)),
-          blur: 4,
-          overlay: Padding(
-            padding: const EdgeInsets.all(13.0),
-            child: Image.asset(
-              "assets/images/search_homepage.png",
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Container(
+            height: 60,
+            width: 60,
+            color: const Color.fromARGB(150, 0, 0, 0),
+          ).blurred(
+            colorOpacity: 0.2,
+            borderRadius:
+                const BorderRadius.horizontal(right: Radius.circular(20)),
+            blur: 4,
+            overlay: Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Image.asset(
+                "assets/images/search_homepage.png",
+              ),
             ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Stack(alignment: AlignmentDirectional.topStart, children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //swiper
-                const HomeSwiper(),
-                const SizedBox(
-                  height: 15,
-                ),
-
-                //list view design
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _setView(true);
-                        });
-                      },
-                      child: const Text('row view'),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _setView(false);
-                        });
-                      },
-                      child: const Text('column view'),
-                    ),
-                  ],
-                ),
-
-                //trending
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: CardTitle(
-                    title: "تراندينغ",
+        body: SafeArea(
+          child: Stack(alignment: AlignmentDirectional.topStart, children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //swiper
+                  const HomeSwiper(),
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CarouselSlider.builder(
-                  options: CarouselOptions(
-                    viewportFraction: 1,
-                    aspectRatio: 2 / 3,
-                    enableInfiniteScroll: false,
-                  ),
-                  itemCount: 3,
-                  itemBuilder: (context, index, realIndex) => CardModule(
-                    listview: listview,
-                    category: "ثقافة وفن",
-                    title:
-                        "مخابرات الجيش وقوّة من مجموعة الشواطئ البحرية التابعة لقوى الأمن …",
-                    date: "مند ساعة",
-                    image: "assets/images/news_small.png",
-                  ),
-                ),
 
-                const SizedBox(
-                  height: 70,
-                ),
+                  //list view design
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _setView(true);
+                          });
+                        },
+                        child: const Text('row view'),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _setView(false);
+                          });
+                        },
+                        child: const Text('column view'),
+                      ),
+                    ],
+                  ),
 
-                //latest news
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: LatestNews(
+                  //trending
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: CardTitle(
+                      title: "تراندينغ",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  CarouselSlider.builder(
+                    options: CarouselOptions(
+                      viewportFraction: 1,
+                      aspectRatio: 2 / 3,
+                      enableInfiniteScroll: false,
+                    ),
+                    itemCount: 3,
+                    itemBuilder: (context, index, realIndex) => CardModule(
                       listview: listview,
-                    )),
+                      category: "ثقافة وفن",
+                      title:
+                          "مخابرات الجيش وقوّة من مجموعة الشواطئ البحرية التابعة لقوى الأمن …",
+                      date: "مند ساعة",
+                      image: "assets/images/news_small.png",
+                    ),
+                  ),
 
-                const SizedBox(
-                  height: 100,
-                ),
+                  const SizedBox(
+                    height: 70,
+                  ),
 
-                // watching now
-                const NewsWatchingNow(),
+                  //latest news
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: LatestNews(
+                        listview: listview,
+                      )),
 
-                const SizedBox(
-                  height: 100,
-                ),
+                  const SizedBox(
+                    height: 100,
+                  ),
 
-                NewsReading(),
-              ],
+                  // watching now
+                  const NewsWatchingNow(),
+
+                  const SizedBox(
+                    height: 100,
+                  ),
+
+                  NewsReading(),
+                ],
+              ),
             ),
-          ),
-          const CustomAppBar(),
-        ]),
+            const CustomAppBar(),
+          ]),
+        ),
       ),
     );
   }
--- lib/widgets/news_reading.dart
+++ lib/widgets/news_reading.dart
@@ -1,3 +1,4 @@
+import 'package:aljadeedapp/widgets/latestNews.dart';
 import 'package:flutter/material.dart';
 
 import '../module/card.dart';
@@ -9,74 +10,135 @@ class NewsReading extends StatefulWidget {
   State<NewsReading> createState() => _NewsReadingState();
 }
 
-class _NewsReadingState extends State<NewsReading>
-    with TickerProviderStateMixin {
+class _NewsReadingState extends State<NewsReading> {
+  final GlobalKey _nowWidgetKey = GlobalKey();
+  final GlobalKey _highlightedWidgetKey = GlobalKey();
+  final GlobalKey _mostReadWidgetKey = GlobalKey();
+
+  double barLeft = 0;
+
+  void _getWidgetInfo(GlobalKey key) {
+    final RenderBox renderBox =
+        key.currentContext?.findRenderObject() as RenderBox;
+
+    final Size size = renderBox.size; // or _widgetKey.currentContext?.size
+    print('Size: ${size.width}, ${size.height}');
+
+    final Offset offset = renderBox.localToGlobal(Offset.zero);
+    print('Offset: ${offset.dx}, ${offset.dy}');
+    print(
+        'Position: ${(offset.dx + size.width) / 2}, ${(offset.dy + size.height) / 2}');
+
+    barLeft = offset.dx;
+  }
+
   @override
   Widget build(BuildContext context) {
-    TabController tabController = TabController(length: 3, vsync: this);
-    int? activeTabIndex = 0;
-
-    activeTabIndex = DefaultTabController.of(context)?.index;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: double.infinity,
-          child: TabBar(
-            labelColor: const Color(0xff111111),
-            unselectedLabelColor: const Color(0xff999999),
-            indicatorColor: const Color(0xff000B27),
-            labelStyle: const TextStyle(
-              fontFamily: 'AlmaraiBold',
-              fontSize: 20,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Color(0xff999999),
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              width: double.maxFinite,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _getWidgetInfo(_nowWidgetKey);
+                      });
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      child: Text(
+                        'الآن',
+                        style: const TextStyle(
+                          fontFamily: 'AlmaraiBold',
+                          fontSize: 20,
+                          color: Color(0xff111111),
+                        ),
+                        key: _nowWidgetKey,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _getWidgetInfo(_highlightedWidgetKey);
+                      });
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      child: Text(
+                        'الأبرز',
+                        style: const TextStyle(
+                          fontFamily: 'AlmaraiBold',
+                          fontSize: 20,
+                          color: Color(0xff111111),
+                        ),
+                        textAlign: TextAlign.center,
+                        key: _highlightedWidgetKey,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _getWidgetInfo(_mostReadWidgetKey);
+                      });
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      child: Text(
+                        'الأكثر قراءة',
+                        style: const TextStyle(
+                          fontFamily: 'AlmaraiBold',
+                          fontSize: 20,
+                          color: Color(0xff111111),
+                        ),
+                        textAlign: TextAlign.center,
+                        key: _mostReadWidgetKey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            tabs: const [
-              Tab(
-                text: 'الآن',
+            SizedBox(
+              height: 100,
+              width: 100,
+            ),
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 100),
+              left: barLeft,
+              top: 61,
+              width: 76,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 4,
+                decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
               ),
-              Tab(
-                text: 'الأبرز',
-              ),
-              Tab(
-                text: 'الأكثر قراءة',
-              ),
-            ],
-            controller: tabController,
-          ),
-        ),
-        Expanded(
-          child: TabBarView(
-            controller: tabController,
-            children: [
-              CardModule(
-                listview: true,
-                category: "ثقافة وفن",
-                title:
-                    "مخابرات الجيش وقوّة من مجموعة الشواطئ البحرية التابعة لقوى الأمن …",
-                date: "مند ساعة",
-                image: "assets/images/news_small.png",
-              ),
-              CardModule(
-                listview: true,
-                category: "ثقافة وفن",
-                title:
-                    "مخابرات الجيش وقوّة من مجموعة الشواطئ البحرية التابعة لقوى الأمن …",
-                date: "مند ساعة",
-                image: "assets/images/news_small.png",
-              ),
-              CardModule(
-                listview: true,
-                category: "ثقافة وفن",
-                title:
-                    "مخابرات الجيش وقوّة من مجموعة الشواطئ البحرية التابعة لقوى الأمن …",
-                date: "مند ساعة",
-                image: "assets/images/news_small.png",
-              ),
-            ],
-          ),
-        ),
-      ],
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
